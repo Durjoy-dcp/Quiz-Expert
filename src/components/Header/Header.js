45fa6d5e3335c694f95aabef74f6ede15254391c
Durjoy-dcp <unknownccxv@gmail.com>
2022-10-11 06:49:49 +0600

    img part done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
+        <div className='d-flex my-5 align-items-center'>
+            <div className='p-5 m-5  w-50'>
+                <h1 className='fs-1 fw-bold'>The online quiz maker that's <br /> easy to use, fun and <br /> fully free</h1>
+            </div>
+            <div className=' border m-5'>
+                <img className='img-fluid' src="ssquiz.png" alt="" />
+            </div>
         </div>
     );
 };
