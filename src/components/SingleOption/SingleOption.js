a24c1c16d1dfaa2c201aba1dfc63c527052508b7
Durjoy-dcp <unknownccxv@gmail.com>
2022-10-11 11:13:03 +0600

    style update radio

--- src/components/SingleOption/SingleOption.js
+++ src/components/SingleOption/SingleOption.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
+import './SingleOption.css';
 
 const SingleOption = ({ option, id, idx }) => {
     return (
         <Col className='p-3 d-flex' sm={6}>
 
-            <input className="form-check-input me-2" type="radio" name={id} id={id + idx} />
-            <label className="  form-check-label" htmlFor={id + idx} >
+            <input className="form-check-input me-2 hideradio" type="radio" name={id} id={id + idx} />
+            <label className="option p-3 text-purple form-check-label w-100 text-center" htmlFor={id + idx} >
                 {option}
             </label>
         </Col>
